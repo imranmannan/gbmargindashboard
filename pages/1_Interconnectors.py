8cecc7ea86af9d5d86575a4905aac8cfdcd71bdc
Imran <imran@personal>
2023-10-26 00:06:17 +0100

    initial commit 2

---
+++ pages/1_Interconnectors.py
@@ -0,0 +1,37 @@
+import plotly.graph_objects as go
+import plotly.express as px
+import streamlit as st
+from utilities import get_IC_flows_from_ENTSOE
+import pandas as pd
+
+# choose date
+event_date_str = st.date_input("Choose Date",key='event_date_str')
+# event_date_str = '2023-09-26'
+
+st.write('# Scheduled IC Flows')
+# get IC flows
+peak_start_str = str(event_date_str) + ' 17:00'
+peak_end_str = str(event_date_str) + ' 18:00' # inclusive, hourly data 
+df = get_IC_flows_from_ENTSOE(peak_start_str,peak_end_str)
+
+# create plotly fig
+net_total_df = df.groupby(['local_dt','time_str'],as_index=False)[['MW']].sum()
+net_total_df['country']   = 'total'
+net_total_df['direction'] = 'net'
+
+# plot overall GB position
+tp1 = pd.concat([df,net_total_df], axis=0)
+fig1 = px.bar(tp1,y='MW',x='direction',color='country', barmode='stack', facet_col='time_str',title=f'{event_date_str}: Net IC Flows during Peak')
+fig1.for_each_annotation(lambda a: a.update(text=a.text.replace("time_str=", ""))) # can make niceer with regex
+
+st.plotly_chart(fig1)
+
+# plot position per IC
+fig2 = px.bar(df, x='country',y='MW',facet_col='time_str', color='country', barmode='stack', title=f'{event_date_str}: IC Flows per IC during Peak')
+fig2.for_each_annotation(lambda a: a.update(text=a.text.replace("time_str=", ""))) # can make niceer with regex
+st.plotly_chart(fig2)
+
+# net_total_df
+# # net_total_df
+# df.groupby(['local_dt'],as_index=False)['MW'].sum()
+# st.write(df)
---
+++ utilities.py
@@ -0,0 +1,90 @@
+import pandas as pd
+# from tqdm.notebook import tqdm
+from stqdm import stqdm as tqdm
+from entsoe import EntsoePandasClient
+from typing import Iterable
+import streamlit as st
+
+# Define your ENTSOE API key
+ENTSOE_API_KEY: str = 'd62efdf1-a73e-499d-a590-bd0be1057b2f'
+
+# Create an ENTSOE client using your API key
+entsoe_client = EntsoePandasClient(api_key=ENTSOE_API_KEY)
+
+# Define a function to get IC flows from ENTSOE
+@st.cache_data
+def get_IC_flows_from_ENTSOE(
+    start_local_dt_str: str,
+    end_local_dt_str: str,
+    IC_country_code_list: Iterable[str] = ['FR', 'NL', 'BE', 'NO_2','GB_NIR'],
+    dayahead: bool = False
+):
+    """
+    Get IC flows from ENTSOE for specified countries and time range.
+
+    Args:
+        start_local_dt_str (str): Start date and time in 'YYYY-MM-DD HH:mm' format.
+        end_local_dt_str (str): End date and time in 'YYYY-MM-DD HH:mm' format.
+        IC_country_code_list (Iterable[str], optional): List of country codes. Defaults to ['FR', 'NL', 'BE', 'NO_2'].
+        dayahead (bool, optional): Whether to fetch day-ahead commercial schedules only, or incude within day. Defaults to False.
+
+    Returns:
+        pd.DataFrame: DataFrame containing IC flows data.
+    """
+
+    # Convert input strings to timestamps with a specified timezone
+    start = pd.Timestamp(start_local_dt_str, tz='Europe/London')
+    end = pd.Timestamp(end_local_dt_str, tz='Europe/London')
+
+    exports: pd.DataFrame = pd.DataFrame()
+    imports: pd.DataFrame = pd.DataFrame()
+
+    # Loop through the specified country codes to fetch import and export data
+    for country in tqdm(IC_country_code_list):
+        imports[f"{country}"] = entsoe_client.query_scheduled_exchanges(
+            country_code_from=country,
+            country_code_to='GB',
+            start=start,
+            end=end,
+            dayahead=dayahead
+        )
+        exports[f"{country}"] = entsoe_client.query_scheduled_exchanges(
+            country_code_from='GB',
+            country_code_to=country,
+            start=start,
+            end=end,
+            dayahead=dayahead
+        )
+
+    # Unstack the data and prepare DataFrames for imports and exports
+    imports.index = imports.index.tz_convert('Europe/London')
+    imports = imports.unstack().reset_index().rename(
+        columns={'level_0': "country", 'level_1': 'local_dt', 0: 'MW'}
+    )
+    imports['direction'] = 'import'
+
+    exports = exports.unstack().reset_index().rename(
+        columns={'level_0': "country", 'level_1': 'local_dt', 0: 'MW'}
+    )
+    exports['MW'] = exports['MW'] * -1
+    exports['direction'] = 'export'
+
+    # Concatenate import and export DataFrames to create the final DataFrame
+    df = pd.concat([imports, exports], axis=0)
+
+    # REMOVED Calculate net flows for each country at each timestamp
+    # net = df.groupby(['country', 'local_dt'])[['MW']].sum().reset_index()
+    # net['direction'] = 'net'
+    # df = pd.concat([df, net], axis=0)
+
+    # Format the local time as 'HH:MM'
+    df['time_str'] = df['local_dt'].dt.strftime('%H:%M')
+
+    return df
+
+# # Get IC flows data for a specific time range
+# start_time = '2023-10-16 17:00'
+# end_time   = '2023-10-16 18:00'
+# df = get_IC_flows_from_ENTSOE(start_time, end_time)
+# You can now work with the 'df' DataFrame for further analysis or visualization.
+
