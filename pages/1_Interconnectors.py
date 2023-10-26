683b2d018bbade3ad8f70a5355c83fdf00a1c700
Imran <imran@personal>
2023-10-26 02:27:28 +0100

    adding totals to stack plot

--- pages/1_Interconnectors.py
+++ pages/1_Interconnectors.py
@@ -3,6 +3,7 @@ import plotly.express as px
 import streamlit as st
 from utilities import get_IC_flows_from_ENTSOE
 import pandas as pd
+import numpy as np
 
 # choose date
 event_date_str = st.date_input(
@@ -27,6 +28,27 @@ net_total_df['direction'] = 'net'
 tp1 = pd.concat([df,net_total_df], axis=0)
 fig1 = px.bar(tp1,y='MW',x='direction',color='country', barmode='stack', facet_col='time_str',title=f'{event_date_str}: Net IC Flows during Peak')
 fig1.for_each_annotation(lambda a: a.update(text=a.text.replace("time_str=", ""))) # can make niceer with regex
+
+#  add sum totals to bars
+tp1_sum = tp1.groupby(['time_str','direction'], as_index=False)['MW'].sum()
+for index, time_str in enumerate(tp1_sum['time_str'].unique()):
+    tp1_sum_time_str = tp1_sum.loc[tp1_sum.time_str == time_str]
+    tp1_sum_time_str['text position'] = np.sign(tp1_sum_time_str['MW']).map({-1:'bottom center',1:'top center'})
+    trace = go.Scatter(
+        x=tp1_sum_time_str['direction'], 
+        y=tp1_sum_time_str['MW'],
+        text=tp1_sum_time_str['MW'],
+        mode='text',
+        textposition=tp1_sum_time_str['text position'],
+        textfont=dict(
+            size=18,
+        ),
+        showlegend=False,
+        )
+    fig1.add_trace(trace, col=index+1, row='all')
+
+# bring sum totals to front of chart
+fig1.data = fig1.data[::-1]
 
 st.plotly_chart(fig1)
 
